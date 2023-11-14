05ce19e9fa3c11df2dfd81b15938c3cc4dec378d
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-11-14 11:40:56 -0500

    Delete frontend directory

--- frontend/src/components/ErrorMsg.jsx
+++
@@ -1,9 +0,0 @@
-function ErrorMsg({ error }) {
-  return (
-    <div>
-      {error && <p className="error-message">{error}</p>}
-      <h2 className="centered-header"></h2>
-    </div>
-  );
-}
-export default ErrorMsg;
--- frontend/src/components/item-creation/conditions/ConditionOptions.jsx
+++
@@ -1,8 +0,0 @@
-const ConditionOptions = [
-  { value: "new", label: "New" },
-  { value: "used", label: "Used" },
-  { value: "refurbished", label: "Refurbished" },
-  { value: "open-box", label: "Open Box" },
-];
-
-export default ConditionOptions;
